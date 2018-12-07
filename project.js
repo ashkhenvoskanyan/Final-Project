85d3cc5fe92b5bb122888ae7eb114d0450126406
ashkhenvoskanyan <voskanyanashxen@gmail.com>
2018-12-07 23:39:54 +0400

    first commit

---
+++ project.js
@@ -0,0 +1,176 @@
+const questions = myQuestions;
+
+const all = function(){
+
+  console.log(questions);
+
+  function Random() {
+        let display = questions;
+        let questionAmount = 10;
+        let questionTracker = [];
+        let randomQuestion;
+        for (let i = 0; i < questionAmount; i++) {
+          do {
+           randomQuestion = Math.floor(Math.random() * display.length);
+             } while (existingQuestions());
+
+            // display.innerHTML = display[randomQuestion] + '<br>';
+            questionTracker.push(randomQuestion);
+            console.log(questionTracker);
+
+
+
+         }
+
+
+     function existingQuestions() {
+          for (let i = 0; i < questionTracker.length; i++) {
+            if (questionTracker[i] === randomQuestion) {
+              return true;
+             }
+            }
+            return false;
+        }
+        return questionTracker
+    };
+
+
+  function buildQuiz(){
+  // we'll need a place to store the HTML output
+    const output = [];
+  // for each question
+
+    
+
+    questions.forEach(
+      function(currentQuestion,questionNumber){
+        const answers = [];
+        // we'll want to store the list of answers choices
+        for(let letter in currentQuestion.answers){ 
+  //for/in - loops through the properties of an object
+  // at that time currentQuestion is object with the answers
+  // ... add an HTML radio button
+  answers.push(
+    `<label>
+          <input type = "radio" name = "question${questionNumber}" value = "${letter}">
+           ${letter} :
+           ${currentQuestion.answers[letter]}
+    </label>`
+    );
+  // When a user clicks on a radio-button, it becomes checked, and all other radio-buttons with equal name become unchecked
+}
+  output.push(
+    `<div class = "slide">
+          <div class = "question"> ${currentQuestion.question} </div>
+          <div class = "answers"> ${answers.join("")}</div>
+     </div>`
+    );
+      });
+
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join("");
+  //The join() method joins the elements of an array into a string, and returns the string.
+}
+
+  function showResults(){
+   
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    let numCorrect = 0;
+
+    questions.forEach(function(currentQuestion,questionNumber){
+
+        const rightAnswers = answerContainers[questionNumber];
+        const selector = `input[name = question${questionNumber}]:checked`;
+        const userAnswer = (rightAnswers.querySelector(selector) || {}).value;
+  //The value property sets or returns the value of the value attribute of a text field.
+
+    if  (userAnswer === currentQuestion.correctAnswer){
+
+      numCorrect ++;
+
+      answerContainers[questionNumber].style.color = "green";
+    
+    }
+    
+    else{
+
+      answerContainers[questionNumber].style.color = "red";
+    
+    }
+  });
+
+  resultsContainer.innerHTML = `${numCorrect} out of 10`;
+  }
+
+  function showSlide(n){
+    const currentQuestionNumber = questionTracker[currentSlide];
+    const nextQuestionNumber = questionTracker[n];
+    slides[currentQuestionNumber].classList.remove("active-slide");
+    slides[nextQuestionNumber].classList.add("active-slide");
+    currentSlide = n;
+    if (currentSlide === 0){
+      previousButton.style.display = "none";
+    }
+    else{
+      previousButton.style.display = "inline-block";
+    }
+    if (currentSlide === questionTracker.length - 1){
+      nextButton.style.display = "none";
+      submitButton.style.display = "inline-block";
+    }
+    else{
+      nextButton.style.display = "inline-block";
+      submitButton.style.display = "none";
+    }
+  }
+
+  function showNextSlide(){
+      showSlide(currentSlide+1);
+    }
+  function showPreviousSlide(){
+      showSlide(currentSlide-1);
+    }
+
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+
+
+buildQuiz();
+
+const questionTracker = Random();
+
+
+
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+showSlide(0);
+
+const left = 37;
+const right = 39;
+const enter = 13
+
+document.addEventListener("keydown", function(event){
+  if(event.keyCode === right){
+    event.preventDefault();
+    showNextSlide();
+  } else if(event.keyCode === left){
+    event.preventDefault();
+    showPreviousSlide();
+  } else if(event.keyCode === enter){
+    event.preventDefault();
+    showResults();
+  }
+}, false);
+
+submitButton.addEventListener('click', showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+
+};
+
+all();
---
+++ questions.js
@@ -0,0 +1,454 @@
+
+const myQuestions = [
+    {
+    question: "What year \"Pirates of the Caribbean: The Curse of the Black Pearl\" released?",
+    answers: {
+      a: "2005",
+      b: "2003",
+      c: "2009",
+      d: "2007"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Which of the followings is not a professor from Hogwarts?",
+    answers: {
+      a: "Severus Snape",
+      b: "Gilderoy Lockhart",
+      c: "Rouben Meschian",
+      d: "Rubeus Hagrid"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who said, \"My Precious?\"",
+    answers: {
+      a: "Frodo",
+      b: "Gollum",
+      c: "Michael Bashan",
+      d: "Biblo"
+    },
+    correctAnswer: "b"
+  },
+    {
+    question: "Which of the followings is not Steven Spielberg movie?",
+    answers: {
+      a: "Transformers",
+      b: "Jaws",
+      c: "Jurassic World",
+      d: "Balto"
+    },
+    correctAnswer: "c"
+  },
+    {
+    question: "Which movie won one big Oscar and seven little ones?",
+    answers: {
+      a: "Snow White",
+      b: "Cinderella",
+      c: "Sleeping Beauty",
+      d: "C'mon It's Obvious"
+    },
+    correctAnswer: "a"
+  },
+    {
+    question: "Which one is Walt Disney's last work?",
+    answers: {
+      a: "The Jungle Book",
+      b: "Sleeping Beauty",
+      c: "Mary Poppins",
+      d: "The Aristocats"
+    },
+    correctAnswer: "a"
+  },
+   {
+    question: "Who received Oscar after his death?",
+    answers: {
+      a: "Paul Walker",
+      b: "Robin Williams",
+      c: "Heath Ledger",
+      d: "Michael Jackson"
+    },
+    correctAnswer: "c"
+  },
+   {
+    question: "Put in the right order:",
+    answers: {
+      a: "Billionaire, genius, playboy, philathropist",
+      b: "Genius, playboy, billionaire, philathropist",
+      c: "Genius, billionaire, playboy, philathropist",
+      d: "Playboy, billionaire, genius, philathropist"
+    },
+    correctAnswer: "c"
+  },
+   {
+    question: "Who does the voice over for Dory from \"Finding Nemo\" and \"Finding Dory\"?",
+    answers: {
+      a: "Ellen Degeneres",
+      b: "Zaruhi Hakobyan",
+      c: "Kate Blanchett",
+      d: "Kate Winslet"
+    },
+    correctAnswer: "a"
+  },
+   {
+    question: "What famous actor is known for the following phrase \"I'll be back\"?",
+    answers: {
+      a: "Jan Klod Van Dam",
+      b: "Arnold Schwarzenegger",
+      c: "Vazgen Barfyan",
+      d: "Jason Statham"
+    },
+    correctAnswer: "b"
+  },
+   {
+    question: "What was the name of the monkey in the Disney movie \"Aladdin\"?",
+    answers: {
+      a: "Kai",
+      b: "Abu",
+      c: "Zazu",
+      d: "Abi"
+    },
+    correctAnswer: "b"
+  },
+   {
+    question: "What is the only mythological animal mentioned in a Harry Potter book and movie title?",
+    answers: {
+      a: "Phoenix",
+      b: "Dragon",
+      c: "Griffin",
+      d: "Basilisk"
+    },
+    correctAnswer: "a"
+  },
+   {
+    question: "Which horror movie character is a serial killer possessing the doll?",
+    answers: {
+      a: "Annabelle",
+      b: "Billy",
+      c: "Robert the Doll",
+      d: "Chucky"
+    },
+    correctAnswer: "d"
+  },
+   {
+    question: "Which character killed people in their dreams?",
+    answers: {
+      a: "Chucky",
+      b: "Jason Voorhees",
+      c: "Freddy Krueger",
+      d: "Pennywise"
+    },
+    correctAnswer: "c"
+  },
+   {
+    question: "Which singer starred in \"The Bodyguard\"?",
+    answers: {
+      a: "Tina Turner",
+      b: "Janet Jackson",
+      c: "Whitney Houston",
+      d: "Lavliii"
+    },
+    correctAnswer: "c"
+  },
+   {
+    question: "Who does the voice over for Woddy from \"Toy Story\" trilogy?",
+    answers: {
+      a: "Woddy Harrelson",
+      b: "Jim Carrey",
+      c: "Robin Williams",
+      d: "Tom Hanks"
+    },
+    correctAnswer: "d"
+  },
+   {
+    question: "Who is the author of the songs from \"The Lion King\"?",
+    answers: {
+      a: "Stevie Wonder",
+      b: "Paul McCartney",
+      c: "Elton John",
+      d: "Linoel Richie"
+    },
+    correctAnswer: "c"
+  },
+   {
+    question: "Who said \"Why So Serious?\"",
+    answers: {
+      a: "Joker from Batman(1989)",
+      b: "Joker from The Dark Knight",
+      c: "Joker from Batman(1966)",
+      d: "Too simple"
+    },
+    correctAnswer: "b"
+  },
+   {
+    question: "In \"The Lion King\", what was Simba's father's name?",
+    answers: {
+      a: "Shere Khan",
+      b: "Jafar",
+      c: "Scar",
+      d: "Mufasa"
+    },
+    correctAnswer: "d"
+  },
+   {
+    question: "What is the name of the magical board game that can only be stopped by finishing the game?",
+    answers: {
+      a: "Jumanji",
+      b: "Ouija",
+      c: "Zathura",
+      d: "Bang!"
+    },
+    correctAnswer: "a"
+  },
+   {
+    question: "Which song plays in every Disney movie opening scene?",
+    answers: {
+      a: "When i wish upon a star from Pinocchio",
+      b: "Once upon a dream from Sleeping Beauty",
+      c: "A dream is a wish your heart makes from Cinderella",
+      d: "I don't know"
+    },
+    correctAnswer: "a"
+  },
+   {
+    question: "Which movie won the Academy Award in 2017?",
+    answers: {
+      a: "La La Land",
+      b: "Moonlight",
+      c: "Arrival",
+      d: "I don't know"
+    },
+    correctAnswer: "b"
+  },
+ {
+    question: "Which 1997 science fiction movie starring Will Smith and Tommy Lee Jones was released?",
+    answers: {
+      a: "Alien: Resurrection",//
+      b: "The Fifth Element",//
+      c: "Men in Black",
+      d: "I don't know"
+    },
+    correctAnswer: "c"
+  },
+   {
+    question: "Which movie is shown in TV every New Year?",
+    answers: {
+      a: "Nightmare before Christmas",
+      b: "Home Alone",
+      c: "How Grinch Stole Christmas",
+      d: "C'mon it's Obvious"
+    },
+    correctAnswer: "b"
+  },
+   {
+    question: "Whose iconic phrase is \"I'm gonna make him an offer he can't refuse\"?",
+    answers: {
+      a: "Vito Corleone",
+      b: "Michael Corleone",
+      c: "Sonny Corleone",
+      d: "Gitenq Don Corleone, but which one"
+    },
+    correctAnswer: "a"
+  },
+   {
+    question: "In which year were the Academy Awards, or Oscar first presented?",
+    answers: {
+      a: "1956",
+      b: "1929",
+      c: "1951",
+      d: "1953"
+    },
+    correctAnswer: "b"
+  },
+   {
+    question: "How many Oscars won \"Avatar\"?",
+    answers: {
+      a: "9",
+      b: "10",
+      c: "11",
+      d: "Many"
+    },
+    correctAnswer: "a"
+  },
+   {
+    question: "Both Anthony Hopkins and Antonio Banderas portrayed which hero? ",
+    answers: {
+      a: "Zorro",
+      b: "Odin",
+      c: "Nestor Castillo",
+      d: "I don't know"
+    },
+    correctAnswer: "a"
+  },
+   {
+    question: "Which actor had to play Tony Stark from \"Iron Man\"?",
+    answers: {
+      a: "Nicolas Cage",
+      b: "Brad Pitt",
+      c: "Tom Cruise",
+      d: "Tom and Nicolas(thank god they are not)"
+    },
+    correctAnswer: "d"
+  },
+   {
+    question: "Who was supposed to play CAPTAIN Jack Sparrow?",
+    answers: {
+      a: "Jim Carrey",
+      b: "Michael Keaton",
+      c: "Christopher Walken",
+      d: "All of them"
+    },
+    correctAnswer: "d"
+  },
+   {
+    question: "How many mistakes NASA managers have found in \"Armageddon\"?",
+    answers: {
+      a: "at least 168",
+      b: "about 153",
+      c: "exactly 67",
+      d: "nearly 141"
+    },
+    correctAnswer: "a"
+  },
+   {
+    question: "In which movie main character said \" My mama always said: Life is like a box of chocolate. You never know what you're gonna get\"?",
+    answers: {
+      a: "The Green Mile",
+      b: "Forrest Gump",
+      c: "Philadelphia",
+      d: "Saving Private Ryan"
+    },
+    correctAnswer: "b"
+  },
+   {
+    question: "The creators of \"Rick and Morty\" were influenced from which movie?",
+    answers: {
+      a: "Doomsday",
+      b: "Back to the Future",
+      c: "Star Trek",
+      d: "The Time Machine"
+    },
+    correctAnswer: "b"
+  },
+   {
+    question: "Which is the first computer-animated feature film?",
+    answers: {
+      a: "Casper",
+      b: "Toy Story",
+      c: "Shrek",
+      d: "A Bug's Life"
+    },
+    correctAnswer: "a"
+  },
+   {
+    question: "What type of fish is Dory from the \"Finding Nemo\"?",
+    answers: {
+      a: "Damselfish",
+      b: "Clownfish",
+      c: "Blue Tang Fish",
+      d: "Bluefish"
+  },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who played James Bond in \"Live and Let Die\"?",
+    answers: {
+      a: "Armen Hajyan",
+      b: "Roger Monroe",
+      c: "Sean Connery",
+      d: "Pierce Brosnan"
+  },
+    correctAnswer: "b"
+  },
+  {
+    question: "What is the name of the actor who played Ron Weasley in the Harry Potter movie series?",
+    answers: {
+      a: "Rupert Grint",
+      b: "Daniel Radcliffe",
+      c: "Tom Felton",
+      d: "Alan Rickman"
+  },
+    correctAnswer: "a"
+  },
+   {
+    question: "How many \"Rocky\" films did Sylvester Stallone star in?",
+    answers: {
+      a: "3",
+      b: "5",
+      c: "6",
+      d: "4"
+  },
+    correctAnswer: "c"
+  },
+   {
+    question: "Which famous actress was born Nomra Jeane Mortenson?",
+    answers: {
+      a: "Audrey Hepburn",
+      b: "Lady Gaga",
+      c: "Natalie Portman",
+      d: "Marilyn Monroe"
+  },
+    correctAnswer: "d"
+  },
+  {
+    question: "From which movie following quote from, \"To infinity and beyond\"?",
+    answers: {
+      a: "The Avengers: Infinity war",
+      b: "Toy Story",
+      c: "The Man Who Knew Infinity",
+      d: "Armageddon"
+  },
+    correctAnswer: "b"
+  },
+  {
+    question: "Tom Hardy won an Oscar for Best Actor in which 2008 movie?",
+    answers: {
+      a: "Bronson",
+      b: "Legend",
+      c: "The Dark Knight Rises",
+      d: "The Revenant"
+  },
+    correctAnswer: "a"
+  },
+  {
+    question: "In the movie \"Prison Break\" why does Michael go to jail?",
+    answers: {
+      a: "Get to know his brother better",
+      b: "Because he wanted to",
+      c: "He wanted to help his brother escape",
+      d: "He wanted to help his cousin escape"
+  },
+    correctAnswer: "d"
+  },
+  {
+    question: "From which movie do the characters Sulley and Mike come from?",
+    answers: {
+      a: "Monsters Inc.",
+      b: "The Incredibles",
+      c: "Ratatouille",
+      d: "The Good Dinosaur"
+  },
+    correctAnswer: "a"
+  },
+  {
+    question: "Who did not play in The \"The Expendables 3\"",
+    answers: {
+      a: "Mel Gibson",
+      b: "Sylvester Stallone",
+      c: "Jackie Chan",
+      d: "Antonio Banderas"
+  },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which movie's soundtrack is \"Une Vie D'amour\"?",
+    answers: {
+      a: "Be My Husband",
+      b: "Spy",
+      c: "Four Hearts",
+      d: "Teheran 43"
+  },
+    correctAnswer: "d"
+  },
+    
+  ];
